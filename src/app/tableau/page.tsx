2b1f0dc84a5f07d764f0a837218a4e84d2b08c11
DIARY7 <diaryhakeur@gmail.com>
2025-04-04 21:30:29 +0300

    second_commit

--- src/app/tableau/page.tsx
+++ src/app/tableau/page.tsx
@@ -3,6 +3,7 @@
 import { useState, useEffect, useRef } from "react";
 import Speedometer from "../components/speedometer";
 import FuelGauge from "../components/fuel-jauge";
+import { Activity } from "lucide-react";
 
 
 export default function Dashboard() {
@@ -82,7 +83,7 @@ useEffect(() => {
         //   return Math.min(prevSpeed + acceleration, maxSpeed);
         if (!isAccelerating && prevSpeed > 0) {
           // Decelerate gradually
-          return Math.max(0, prevSpeed - 1);
+          return Math.max(0, prevSpeed - 0.25);
         }
         return prevSpeed;
       });
@@ -103,29 +104,33 @@ useEffect(() => {
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 p-4">
       <div className="w-full max-w-4xl bg-black rounded-3xl shadow-2xl overflow-hidden border-2 border-gray-800">
         <div className="p-8 relative">
-          <div className="text-gray-200 text-2xl mb-2 text-center ">
+          <div className="text-gray-200 text-2xl mb-2 text-center font-bold" style={{letterSpacing:"2px"}} >
             TABLEAU DE BORD
           </div>
+          
+          <div className="flex flex-col items-center  p-2 rounded-lg border shadow-lg">
+              <div className="w-2x h-1x bg-black rounded-md flex flex-row items-center justify-between py-1 px-3 gap-5 space-x-4 bg-gray-950 border-zinc-500">
+                <div
+                  className={`w-10 h-10 rounded-full ${isDescelering ? "bg-red-500 shadow-md shadow-red-500/50" : "bg-red-900"}`}
+                ></div>
+                <div
+                  className={`w-10 h-10 rounded-full ${isAccelerating ? "bg-green-500 shadow-md shadow-green-500/50" : "bg-green-900"}`}
+                ></div>
+              </div>
+            </div>
+
           <div className="flex flex-col md:flex-row justify-around items-center gap-8 p-4">
             <Speedometer speed={speed} maxSpeed={maxSpeed} />
             <FuelGauge fuelLevel={fuel} />
           </div>
 
           <div className="mt-8 flex justify-center space-x-2.5">
-            <div className="px-3 py-2 text-shadow-md bg-red-600 hover:bg-red-700 rounded-xl shadow-lg text-white text-center ">
-                Acceleration : {acceleration} 
+            <div className="px-3 py-2 text-shadow-md bg-yellow-600 rounded-xl shadow-lg flex text-white text-center ">
+                <Activity className="w-4 h-4 text-white" />
+                <span>Acceleration : {acceleration} km/h</span>
+                <Activity className="w-4 h-4 text-white" /> 
             </div>
-
-            <div className="flex flex-col items-center bg-gray-800 p-2 rounded-lg border border-gray-700 shadow-lg">
-              <div className="w-6 h-14 bg-black rounded-md flex flex-col items-center justify-between p-1">
-                <div
-                  className={`w-4 h-4 rounded-full ${isDescelering ? "bg-red-500 shadow-md shadow-red-500/50" : "bg-red-900"}`}
-                ></div>
-                <div
-                  className={`w-4 h-4 rounded-full ${isAccelerating ? "bg-green-500 shadow-md shadow-green-500/50" : "bg-green-900"}`}
-                ></div>
-              </div>
-            </div>
+            
 
           </div>
 
